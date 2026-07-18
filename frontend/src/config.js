22aede356628a84f11261de6c5312b819855b272
Charan0413 <charanyadav417@gmail.com>
2026-07-18 12:02:34 +0530

    Connect frontend to EKS backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:55049";
+  import.meta.env.VITE_API_URL || "http://a2851965334694b2e8c08cba335de903-1579695090.ap-south-1.elb.amazonaws.com:5000";
